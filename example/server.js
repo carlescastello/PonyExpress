40427fd368860f81cc21922e494d9b98d357b10e
Juan Pablo Rojas <jpablorp@warebeone.com>
2013-04-02 18:28:55 -0500

    Comentarios Server.js

--- example/server.js
+++ example/server.js
@@ -1,3 +1,4 @@
+/* Importar librerias */
 var express  = require('express'),
 	app      = express(),
 	server   = require('http').createServer(app),
@@ -6,33 +7,41 @@ var express  = require('express'),
 	cons     = require('consolidate'),
 	uuid     = require('node-uuid'),
 	messages = [];
-
+/* Escucho por puerto 3000 */
 server.listen(3000);
 console.log('visit http://localhost:3000 for simple example \nvisit http://localhost:3000/backbone for backbone example');
 
+/* Quito cache de swig */
 swig.init({
 	cache : false
 });
 
+/* Arreglo de mensajes */
 var messages = [];
 
+/* Coloco engine con swig */
 app.engine('.html', cons.swig);
 app.set('view engine', 'html');
 
+/* Establesco carpeta estatica */
 app.use(express.static('../'));
 
+/* Habilito solicitudes POST */
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.methodOverride());
 
+/* GET a / */
 app.get('/', function (req, res) {
 	res.render('index');
 });
 
+/* GET de /Backbone */
 app.get('/backbone', function (req, res){
 	res.render('backbone');
 });
 
+/* POST de mensajes enviados */
 app.post('/messages', function (req, res){
 	req.body.id = uuid.v1();
 	console.log('body', req.body);
@@ -44,6 +53,7 @@ app.post('/messages', function (req, res){
 	res.send(200, {status:"Ok"});
 });
 
+/* Delete para cada id de messages */
 app.delete('/messages/:id', function (req, res){
 	var message;
 
@@ -60,6 +70,8 @@ app.delete('/messages/:id', function (req, res){
 	res.send(200, {status:"Ok"});
 });
 
+
+/* POST de Backbone hacia el id de cada mensaje */
 app.put('/messages/:id', function (req, res){
 	var message;
 
@@ -76,10 +88,12 @@ app.put('/messages/:id', function (req, res){
 	res.send(200, {status:"Ok"});
 });
 
+/* GET a messages */
 app.get('/messages', function (req, res) {
 	res.send(messages);
 });
 
+/* Establece eventos create delete y update para cada mensaje en cada socket */
 var connection = function(socket){		
 	socket.on('message::create', function(data){
 		messages.push(data);
@@ -98,7 +112,7 @@ var connection = function(socket){
 
 io.sockets.on('connection', connection);
 
-// Fake client
+/* Inicio Clientes Fake */
 var Faker    = require('Faker'),
 	ioClient = require('socket.io-client'),
 	socket   = ioClient.connect('http://localhost:3000'),
@@ -130,3 +144,4 @@ setInterval(function(){
 
 	socket.emit('message::create', lastMessage);
 },1000);
+/* Fin Clentes Fake */
